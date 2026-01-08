40bbd691bb621ed13ee895785ea388facc548cf5
Yue Cao <yue.cao@sydney.edu.au>
2026-01-08 20:06:09 +1100

    Make CellChat optional

--- R/helper_CCI.R
+++ R/helper_CCI.R
@@ -2,6 +2,13 @@
 # helper function to run CCI 
 helper_CCI <- function( alldata , ncores = 1  ){
  
+  if (!requireNamespace("CellChat", quietly = TRUE)) {
+    cli::cli_abort(c(
+      "CellChat is required to compute CCI features but is not installed.",
+      "i" = "Install it from GitHub, then re-run this feature type."
+    ))
+  }
+  
   BPparam <-  generateBPParam(ncores)
  
   data(LRdb, package = "SingleCellSignalR")
@@ -10,6 +17,7 @@ helper_CCI <- function( alldata , ncores = 1  ){
  
   capture.output( suppressMessages( individual_cci <- BiocParallel::bplapply(  unique(alldata$sample), function(x){
     
+<<<<<<< HEAD
                         data_dataframe  <- alldata$data[, alldata$sample == x, drop=F]
                         
                         celltype <- as.factor( alldata$celltype[ alldata$sample == x])
@@ -37,6 +45,91 @@ helper_CCI <- function( alldata , ncores = 1  ){
                        
                         all_interaction
                }, BPPARAM = BPparam) ) )
+=======
+    err <- try({
+              this_sample_data <- alldata$data[, alldata$sample == x]
+              colnames(  this_sample_data) <- make.unique( colnames(   this_sample_data) ) 
+              
+               
+              meta = data.frame(labels = alldata$celltype[ alldata$sample == x]   )   
+              rownames(meta) <-  colnames(   this_sample_data) 
+          
+              cellchat  <- CellChat::createCellChat(object =  this_sample_data , meta = meta, 
+                                          group.by = "labels")
+          
+              
+              cellchat <- CellChat::setIdent(cellchat, ident.use = "labels") # set "labels" as default cell identity
+              
+              
+             
+              groupSize <- as.numeric(table(cellchat@idents)) # number of cells in each cell group
+           
+              cellchat@DB <- CellChatDB # set the used database in the object
+              
+              cellchat <- CellChat::subsetData(cellchat) # subset the expression data of signaling genes for saving computation cost
+             
+              
+          
+               # do parallel
+              cellchat <- CellChat::identifyOverExpressedGenes(cellchat)
+              cellchat <- CellChat::identifyOverExpressedInteractions(cellchat)
+              cellchat <- CellChat::smoothData(cellchat, adj = PPI)
+              
+            
+              cellchat <- CellChat::computeCommunProb(cellchat)
+              cellchat <- CellChat::computeCommunProbPathway(cellchat)
+              cellchat <- CellChat::aggregateNet(cellchat)
+              
+              
+          
+              cellchat_score <-   CellChat::netVisual_bubble(   cellchat,   return.data = TRUE )   
+              cellchat_score  <- cellchat_score $communication
+              
+            
+              
+                cellchat_score$feature <- paste0(  cellchat_score$source   , "->" , 
+                                                   cellchat_score$target,
+                                                   "--", 
+                                                  cellchat_score$ligand  , "->", 
+                                                  cellchat_score$receptor)
+
+               
+           
+              cellchat_score 
+              
+             
+    })
+    
+    if (class(err) == "try-error"){
+ 
+        cellchat_score <- data.frame(source = "placeholder", 
+                                     target = "placeholder",
+                                     ligand = "placeholder",
+                                     receptor = "placeholder",
+                                     prob =  0,
+                                     pval = 0,
+                                     interaction_name = "placeholder",
+                                     interaction_name_2 = "placeholder",
+                                     pathway_name = "placeholder",
+                                     annotation =  "placeholder",
+                                     evidence =  "placeholder",
+                                     source.target =  "placeholder",
+                                     prob.original =  0,
+                                     feature = "placeholder" )
+        
+        
+        cellchat_score 
+    
+    }else{
+      cellchat_score 
+      }
+  
+       }, BPPARAM = BPparam) ))
+  
+  
+  
+  
+>>>>>>> aa0f2c8 (Make CellChat optional)
    
    
    # gather the cell - cell interaction probability into sample x interaction probability matrix 
